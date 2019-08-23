c5cd73b340029486609630693d2a9b94224012e1
Chris Doran <chrisdoran@protonmail.com>
2019-08-23 20:54:04 +1000

    Add github, linkedin, new cv

--- src/components/header.js
+++ src/components/header.js
@@ -7,18 +7,43 @@ const Header = ({ siteTitle }) => (
   <header
     style={{
       marginBottom: `1.45rem`,
-      display: `flex`,
       paddingLeft: `5rem`,
       paddingRight: `5rem`,
       height: `5rem`,
+      display: `flex`
     }}
   >
-    <h2>
-      {/* Note, this goes to the file hosted in the static folder */}
-      <a href="/curriculum-vitae-christopher-doran.pdf">
-        <Messenger news={['⠠⠠⠉⠧', 'Curriculum Vitae']}/>
-      </a>
+  <div style={{
+    display: 'flex',
+    justifyContent: 'flex-start',
+    width: '50%',
+  }}>  
+    <h2 className='header-item'>
+        {/* Note, this goes to the file hosted in the static folder */}
+        <a href="/curriculum-vitae-christopher-doran.pdf">
+          <Messenger news={['⠠⠠⠉⠧', 'Curriculum Vitae']}/>
+        </a>
+      </h2>
+    </div>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'flex-end',
+      width: '50%',
+    }}>
+
+    <h2 className='header-item'>
+      <a href="https://linkedin.com/in/cdrn/" target="_blank">
+        <Messenger news={['⠇⠔⠅⠫⠔', 'LinkedIn']}/>
+      </a> 
     </h2>
+
+    <h2 className='header-item'>
+      <a href="https://github.com/cdrn/" target="_blank">
+        <Messenger news={['⠛⠊⠹⠥⠃', 'GitHub']}/>
+      </a> 
+    </h2>
+
+    </div>
   
   </header>
 )
