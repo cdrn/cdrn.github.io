e5f3c95b11dbc847d78a107fd9454186265badcc
Chris Doran <chrisdoran@protonmail.com>
2019-08-23 20:06:20 +1000

    add cv

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,24 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Messenger from '../components/messenger'
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `red`,
       marginBottom: `1.45rem`,
       display: `flex`,
       paddingLeft: `5rem`,
       paddingRight: `5rem`
     }}
   >
+    <h2>
+      {/* Note, this goes to the file hosted in the static folder */}
+      <a href="/curriculum-vitae-christopher-doran.pdf">
+        <Messenger news={['⠠⠠⠉⠧', 'Curriculum Vitae']}/>
+      </a>
+    </h2>
+  
   </header>
 )
 
